7da117ef1d7ba27ebe70a2ea1098f4ac513d8b57
Mighty Mike <nanaadams246@gmail.com>
2023-10-03 06:17:13 +0000

    now theme data accessible globally

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import {store} from './reduxConfig/store'
+import { ThemeDataProvider } from "./components/ThemeDataContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ThemeDataProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ThemeDataProvider>
   </Provider>
 );
 
